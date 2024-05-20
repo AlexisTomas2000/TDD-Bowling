65c3f31b48ec183eb63cb9693a89978fc16ee259
AlexisTomas2000 <81593160+AlexisTomas2000@users.noreply.github.com>
2024-05-20 17:26:15 -0300

    Update Bowling.py

--- Bowling.py
+++ Bowling.py
@@ -2,12 +2,15 @@ class Juego:
     def __init__(self, *args):
         pass
 
+    
     def jugar(self):
         pass
+
 
 def main():
     juego = Juego()
     juego.jugar()
+
 
 if __name__ == "__main__":
     main()
