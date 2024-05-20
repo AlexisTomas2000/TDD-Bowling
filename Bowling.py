a82fa32dd4aa8a7a463ebf53707bb94c25b93c4e
AlexisTomas2000 <81593160+AlexisTomas2000@users.noreply.github.com>
2024-05-20 17:17:29 -0300

    Update Bowling.py

--- Bowling.py
+++ Bowling.py
@@ -1,19 +1,13 @@
-class Juego():
-    pass
-    def __init__(self) -> None:
-        self.puntos = 0
-        self.jugada = []
-    def Tirar(self,pinos:int):
-        self.jugada.append(pinos)
-    def Score(self):
-        total = sum(self.jugada)
-        ultimo_tiro = self.jugada[len(self.jugada)-2]
-        penultimo_tiro = self.jugada[len(self.jugada)-3]
-        if ultimo_tiro + penultimo_tiro == 10:
-                total += self.jugada[len(self.jugada)-1]
-        return total
+class Juego:
+    def __init__(self, *args):
+        pass
 
+    def jugar(self):
+        pass
 
+def main():
+    juego = Juego()
+    juego.jugar()
 
-"""def __init__(self,a) -> None:
-    self.jugada = a"""
+if __name__ == "__main__":
+    main()
