04f69c3bad8d30bf4ab5d2812b199d2db2aaec52
AlexisTomas2000 <81593160+AlexisTomas2000@users.noreply.github.com>
2024-05-20 17:38:02 -0300

    Update test_bowling.py

--- test_bowling.py
+++ test_bowling.py
@@ -1,9 +1,10 @@
-import Bowling 
+import Bowling
 
+jugada = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 1, 1]
 
-jugada = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,9,1,1]
 def test_Spare():
-    jogo = Juego()
+    jogo = Bowling.Juego()  # Corrección del nombre de la clase
     for tiro in jugada:
         jogo.Tirar(tiro)
-    assert jogo.Score()==29
+    assert jogo.Score() == 29
+
